c50f43a4559c54edaefff9b4326873b2f50f5326
Alex Ma <alexjmma@users.noreply.github.com>
2018-11-11 23:41:43 -0800

    finished board

--- Board.cpp
+++ Board.cpp
@@ -1,9 +1,5 @@
 #include "Board.h"
 
-
-
-
-
 void Board::InitBoard()
 {
 	for (int i = 0; i < board_width; i++)
@@ -76,4 +72,49 @@ void Board::DeletePossibleLines()
 
 		if (i == board_width) DeleteLine(j);
 	}
+}
+
+//Determines if the block of the board is full or empty
+bool Board::IsFreeBlock(int pX, int pY)
+{
+	if (mBoard[pX][pY] == POS_FREE)
+		return true;
+	else
+		return false;
+}
+
+//pPos = Horizontal Position of block on the board
+int Board::GetXPosInPixels(int pPos)
+{
+	return ((board_position - (block_size * (board_width / 2))) + (pPos * block_size));
+}
+
+int Board::GetYPosInPixels(int pPos)
+{
+	return ((mScreenHeight - (block_size * board_height)) + (pPos * block_size));
+}
+
+bool Board::IsPossibleMovement(int pX, int pY, int pPiece, int pRotation)
+{
+	//Check collision with other pieces and the border of the board
+	for (int i1 = pX, i2 = 0; i1 < pX + piece_blocks; i1++, i2++)
+	{
+		for (int j1 = pY, j2 = 0; j1 < pY + piece_blocks; j1++, j2++)
+		{
+			//checks if piece is outside the limit of the board
+			if (i1 < 0 || i1 > board_width - 1 || j1 > board_height - 1)
+			{
+				if (mPieces->GetBlockType(pPiece, pRotation, j2, i2) != 0)
+					return 0;
+			}
+			//checks if the pieces collide with a block already stored in the board
+			if (j1 >= 0)
+			{
+				if ((mPieces->GetBlockType(pPiece, pRotation, j2, i2) != 0) && (!IsFreeBlock(i1, j1)))
+					return false;
+			}
+		}
+	}
+	//if not collision return true
+	return true;
 }
---
+++ Game.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef _GAME_
+#define _GAME_
+
+#include "Board.h"
+#include "Pieces.h"
+#include "IO.h"
+#include <time.h>
+
+constexpr int wait_time = 700;
+class Game
+{
+public:
+	Game();
+	~Game();
+};
+
